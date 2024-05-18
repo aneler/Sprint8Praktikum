22205c0c6126be6c070ac98ac50120d832b40dfa
red <elena.nikifortseva@gmail.com>
2024-05-19 00:05:19 +0200

    add steps

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,6 +1,9 @@
 package com.elena;
 
 import static org.junit.Assert.*;
+
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -31,7 +34,31 @@ public class AccountTest {
     @Test
     @Description("Проверка метода Account с разными входными данными")
     public void AccountTest(){
+        logTestParameters(name, expected);
+        checkAcountTest();
+    }
+
+    @Step
+    public void checkAcountTest(){
         Account account = new Account(name);
+        boolean result = account.checkNameToEmboss();
         assertEquals(expected, account.checkNameToEmboss());
+        logTestResult(result);
+    }
+
+    @Step("Входные данные: name = {name}, expected = {expected}")
+    private void logTestParameters(String name, boolean expected) {
+        attachText("Имя", name);
+        attachText("Ожидаемый результат", Boolean.toString(expected));
+    }
+
+    @Step("Результат теста: result = {result}")
+    private void logTestResult(boolean result) {
+        attachText("Фактический результат", Boolean.toString(result));
+    }
+
+    @Attachment(value = "{name}", type = "text/plain")
+    private String attachText(String name, String value) {
+        return value;
     }
 }
